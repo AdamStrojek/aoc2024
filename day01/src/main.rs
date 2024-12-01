a6069e16dba83c3a54a380c16df429ffb0b4c4d2
Adam Strojek <adam@strojek.info>
2024-12-01 11:20:11 +0100

    Day 01: Add solution for part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,18 @@
+use std::collections::HashMap;
 use anyhow::{Context, Result};
 use std::fs;
 
 fn main() -> Result<()> {
-    solve("example.txt")?;
+    solve_part1("example.txt")?;
+    solve_part1("input.txt")?;
+
+    solve_part2("example.txt")?;
+    solve_part2("input.txt")?;
 
     Ok(())
 }
 
-fn solve(file_name: &str) -> Result<u32> {
+fn solve_part1(file_name: &str) -> Result<u32> {
     println!("Processing file {file_name}");
 
     let binding = fs::read_to_string(file_name)?;
@@ -33,4 +38,32 @@ fn solve(file_name: &str) -> Result<u32> {
     println!("Answer: {distance}");
 
     Ok(distance)
+}
+
+
+fn solve_part2(file_name: &str) -> Result<u32> {
+    println!("Processing file {file_name}");
+
+    let binding = fs::read_to_string(file_name)?;
+    let lines = binding.lines();
+
+    let mut list: Vec<u32> = vec![];
+    let mut map: HashMap<u32, u32> = HashMap::new();
+
+    for line in lines {
+        let mut it = line.split_whitespace();
+        let a = it.next().context("Missing a in line")?.parse()?;
+        let b = it.next().context("Missing b in line")?.parse()?;
+
+        list.push(a);
+        map.entry(b)
+            .and_modify(|val| *val += 1)
+            .or_insert(1u32);
+    }
+
+    let answer = list.into_iter().map(|a| a * map.get(&a).unwrap_or(&0)).sum();
+
+    println!("Answer: {answer}");
+
+    Ok(answer)
 }
