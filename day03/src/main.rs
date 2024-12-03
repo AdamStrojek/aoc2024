c23aaf4da1fe9a0d05540b24f0524094ea4e4fbb
Adam Strojek <adam@strojek.info>
2024-12-03 10:57:20 +0100

    Day 03: Part 1 solution

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use regex::Regex;
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    solve("example.txt")?;
+    solve("input.txt")?;
+
+    Ok(())
+}
+
+fn solve(file_name: &str) -> Result<i32> {
+    let content = fs::read_to_string(file_name)?;
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)")?;
+
+    let mut result = 0;
+
+    for capture in re.captures_iter(&content) {
+        let a: i32 = capture.get(1).context("Can't get group")?.as_str().parse()?;
+        let b: i32 = capture.get(2).context("Can't get group")?.as_str().parse()?;
+        result += a * b;
+    }
+
+    println!("Result: {result}");
+
+    Ok(result)
 }
