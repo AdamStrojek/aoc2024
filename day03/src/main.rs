123cd7b58dc317906cc0d952c15719866fd34afd
Adam Strojek <adam@strojek.info>
2024-12-03 11:58:17 +0100

    Day 03: Part 2 solution

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,13 +3,16 @@ use regex::Regex;
 use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
-    solve("example.txt")?;
-    solve("input.txt")?;
+    solve_part1("example.txt")?;
+    solve_part1("input.txt")?;
+
+    solve_part2("example2.txt")?;
+    solve_part2("input.txt")?;
 
     Ok(())
 }
 
-fn solve(file_name: &str) -> Result<i32> {
+fn solve_part1(file_name: &str) -> Result<i32> {
     let content = fs::read_to_string(file_name)?;
 
     let re = Regex::new(r"mul\((\d+),(\d+)\)")?;
@@ -20,6 +23,38 @@ fn solve(file_name: &str) -> Result<i32> {
         let a: i32 = capture.get(1).context("Can't get group")?.as_str().parse()?;
         let b: i32 = capture.get(2).context("Can't get group")?.as_str().parse()?;
         result += a * b;
+    }
+
+    println!("Result: {result}");
+
+    Ok(result)
+}
+
+
+fn solve_part2(file_name: &str) -> Result<i32> {
+    let content = fs::read_to_string(file_name)?;
+
+    let re = Regex::new(r"(mul|do|don't)\(((\d+),(\d+))?\)")?;
+
+    let mut result = 0;
+    let mut disable = false;
+
+    for capture in re.captures_iter(&content) {
+        let x = capture.get(1).context("Can't get operation")?.as_str();
+        match x {
+            "mul" => if !disable {
+                if let (Some(a), Some(b)) = (capture.get(3).and_then(|x| Some(x.as_str())), capture.get(4).and_then(|x| Some(x.as_str()))) {
+                    result += a.parse::<i32>()? * b.parse::<i32>()?;
+                }
+            },
+            "do" => if capture.get(2).is_none() {
+                disable = false;
+            }
+            "don't" => if capture.get(2).is_none() {
+                disable = true
+            },
+            _ => {panic!("Ignoring {x}")},
+        };
     }
 
     println!("Result: {result}");
